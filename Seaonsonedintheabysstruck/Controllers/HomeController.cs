88c71bff598deb5655a95f8d11f82fa8eec91ca7
Jennifer Flores <jlflores@charter.net>
2019-01-26 12:45:39 -0600

    calendar page loaded

--- Seaonsonedintheabysstruck/Controllers/HomeController.cs
+++ Seaonsonedintheabysstruck/Controllers/HomeController.cs
@@ -37,6 +37,10 @@ namespace Seaonsonedintheabysstruck.Controllers
         {
             return View();
         }
+        public IActionResult Calendar()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
